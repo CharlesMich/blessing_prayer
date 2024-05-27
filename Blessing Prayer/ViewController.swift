c7bf7e279f9241b06ec3bdb668d75ee4150d48e7
CharlesMich <charleedongo@hotmail.com>
2024-05-27 13:59:26 -0500

    Initial Commit

---
+++ Blessing Prayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blessing Prayer
+//
+//  Created by Charles Michael on 5/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
